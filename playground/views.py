35842cc2b7f9fb9c0c13c00eb134179015f68f33
Pavel <pavel.timofeev2008@gmail.com>
2024-04-13 10:39:23 -0700

    First commit

---
+++ playground/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+#request -> response
+#request handler
+#action
+
+def calculate():
+    x = 1
+    y = 2
+    return x
+
+def say_hello(request):
+    #Pull data from db
+    # Transform data
+    #Send email\
+    #return HttpResponse("Hello World!")
+    x = calculate()
+    return render(request, 'hello.html',{'name':'Pavel!'})
